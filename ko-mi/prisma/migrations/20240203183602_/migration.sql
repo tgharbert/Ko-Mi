298331acc7ebfcb7b6cd23d1b18e5d44d61ae61c
Thomas Harbert <tgharbert3@gmail.com>
2024-02-12 17:03:26 -0600

    removed subfolder

--- ko-mi/prisma/migrations/20240203183602_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ReportedURL" (
-    "id" SERIAL NOT NULL,
-    "url" TEXT NOT NULL,
-    "addressed" BOOLEAN NOT NULL,
-
-    CONSTRAINT "ReportedURL_pkey" PRIMARY KEY ("id")
-);
