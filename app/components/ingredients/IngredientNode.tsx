4a25debf4394b61066e19891924760a7af322389
Thomas Harbert <tgharbert3@gmail.com>
2024-02-16 16:31:16 -0600

    further ingredient node styling

--- app/components/ingredients/IngredientNode.tsx
+++ app/components/ingredients/IngredientNode.tsx
@@ -13,21 +13,8 @@ const IngredientNode = ({ ingredient }: { ingredient: Ingredient }) => {
   };
 
   return (
-    // <div className=" mx-4 pb-3 float-left block">
     <span className="sm:flex sm:items-center sm:justify-center ">
       <li className="flexbox overflow-y-auto mx-4 px-4 text-left border-2 sm:w-2/5 border-black rounded-lg h-auto mt-4 bg-tertiary text-black">
-        {/* <span className="mt-6 flex w-1/5">
-          <IconButton
-            onClick={() => onCheckIngredient()}
-            className="justify-center content-center flex"
-          >
-            {isChecked ? (
-              <RemoveShoppingCartIcon className=" text-red-500 text-left flex justify-center content-center" />
-            ) : (
-              <AddShoppingCartIcon className=" text-lime-500 float-left" />
-            )}
-          </IconButton>
-        </span> */}
         <p
           className={
             isChecked
@@ -35,23 +22,19 @@ const IngredientNode = ({ ingredient }: { ingredient: Ingredient }) => {
               : "w-full float-center text-lg "
           }
         >
-          <span className="mt-4 mb-4 flex ">
-            <IconButton
-              onClick={() => onCheckIngredient()}
-              className="justify-center content-center flex"
-            >
+          <span className="mt-4 mb-4 flex">
+            <IconButton onClick={() => onCheckIngredient()} className="">
               {isChecked ? (
-                <RemoveShoppingCartIcon className=" text-red-500 text-left flex justify-center content-center mr-4" />
+                <RemoveShoppingCartIcon className=" text-red-500 text-left" />
               ) : (
-                <AddShoppingCartIcon className=" text-lime-500 float-left mr-4" />
+                <AddShoppingCartIcon className=" text-lime-500 float-left " />
               )}
             </IconButton>
-            <div>{ingredient.name}</div>
+            <div className="px-20">{ingredient.name}</div>
           </span>
         </p>
       </li>
     </span>
-    // </div>
   );
 };
 
