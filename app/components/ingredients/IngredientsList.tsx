84b493b7d81eb9ac8b1e6c9881872a5837ab63ad
Thomas Harbert <tgharbert3@gmail.com>
2024-02-16 15:16:34 -0600

    restyled buttons

--- app/components/ingredients/IngredientsList.tsx
+++ app/components/ingredients/IngredientsList.tsx
@@ -4,10 +4,40 @@ import IngredientNode from "./IngredientNode";
 import LoadingPage from "../Loading";
 import ClearIcon from "@mui/icons-material/Clear";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { deleteCheckedIngredients } from "@/lib/deleteCheckedIngredients";
 import { deleteUserIngredients, getUserIngredients } from "@/lib/ingredients";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+
+// should extract this into another folder and export 'theme'
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    lime: Palette["primary"];
+  }
+
+  interface PaletteOptions {
+    lime?: PaletteOptions["primary"];
+  }
+}
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    lime: true;
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    lime: {
+      main: "#84CC16",
+      light: "#65A30D",
+      dark: "#65A30D",
+      contrastText: "#F7F9F9",
+    },
+  },
+});
 
 const IngredientsList = () => {
   const [ingredients, setIngredients] = useState([]);
@@ -45,61 +75,68 @@ const IngredientsList = () => {
   };
 
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <div>
-        <div className="pb-8">
-          <button
+        <div>
+          <div className="pb-8">
+            {/* <button
             onClick={handleDeleteChecked}
             className=" mr-4 bg-lime-500 rounded px-3"
-          >
+            >
             <ClearIcon />
             Delete Checked
-          </button>
-          {/* Adjust this with MUI button! */}
-          {/* <Stack
-            // spacing={2}
-            direction="row"
-            className="flex justify-center content-center px-4"
-          >
-            <Button
-              variant="contained"
-              className="px-4 bg-lime-500"
-              onClick={handleDeleteChecked}
+          </button> */}
+            {/* Adjust this with MUI button! */}
+            <Stack
+              // spacing={2}
+              direction="row"
+              className="flex justify-center content-center px-4"
             >
-              Delete Checked
-            </Button>
-            <Button
-              variant="contained"
-              className="bg-lime-500"
-              onClick={handleDeleteIngredients}
-            >
-              Delete All
-            </Button>
-          </Stack> */}
-          <button
+              <Button
+                variant="contained"
+                className="px-4 bg-lime-500"
+                onClick={handleDeleteChecked}
+                color="lime"
+              >
+                Delete Checked
+              </Button>
+              <Button
+                variant="contained"
+                className="bg-lime-500"
+                onClick={handleDeleteIngredients}
+                color="lime"
+              >
+                Delete All
+              </Button>
+            </Stack>
+            {/* <button
             onClick={handleDeleteIngredients}
             className=" ml-4 bg-lime-500 px-3 rounded"
-          >
+            >
             Delete All Items
             <DeleteIcon />
-          </button>
-        </div>
-        {/* SHOULD BE AN INFINITE SCROLL?? */}
-        {isLoading ? (
-          <LoadingPage />
-        ) : (
-          <div className="flex-col">
-            <ul>
-              {ingredients.map((ingredient: Ingredient) => {
-                return (
-                  <IngredientNode key={ingredient.id} ingredient={ingredient} />
-                );
-              })}
-            </ul>
+          </button> */}
           </div>
-        )}
+          {/* SHOULD BE AN INFINITE SCROLL?? */}
+          {isLoading ? (
+            <LoadingPage />
+          ) : (
+            <div className="flex-col">
+              <ul>
+                {ingredients.map((ingredient: Ingredient) => {
+                  return (
+                    <IngredientNode
+                      key={ingredient.id}
+                      ingredient={ingredient}
+                    />
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 };
 
