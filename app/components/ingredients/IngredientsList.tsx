e6d784829c1b901fdead589f6fb9fda3a5d365a9
Thomas Harbert <tgharbert3@gmail.com>
2024-02-16 13:22:39 -0600

    unsure of buttons to use

--- app/components/ingredients/IngredientsList.tsx
+++ app/components/ingredients/IngredientsList.tsx
@@ -6,6 +6,8 @@ import ClearIcon from "@mui/icons-material/Clear";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { deleteCheckedIngredients } from "@/lib/deleteCheckedIngredients";
 import { deleteUserIngredients, getUserIngredients } from "@/lib/ingredients";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 const IngredientsList = () => {
   const [ingredients, setIngredients] = useState([]);
@@ -53,6 +55,27 @@ const IngredientsList = () => {
             <ClearIcon />
             Delete Checked
           </button>
+          {/* Adjust this with MUI button! */}
+          {/* <Stack
+            // spacing={2}
+            direction="row"
+            className="flex justify-center content-center px-4"
+          >
+            <Button
+              variant="contained"
+              className="px-4 bg-lime-500"
+              onClick={handleDeleteChecked}
+            >
+              Delete Checked
+            </Button>
+            <Button
+              variant="contained"
+              className="bg-lime-500"
+              onClick={handleDeleteIngredients}
+            >
+              Delete All
+            </Button>
+          </Stack> */}
           <button
             onClick={handleDeleteIngredients}
             className=" ml-4 bg-lime-500 px-3 rounded"
