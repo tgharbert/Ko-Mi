f9bbc513994e212c6c4b0feb902be99fd0e74cca
Thomas Harbert <tgharbert3@gmail.com>
2024-02-27 13:26:14 -0600

    90% coverage of parsing function

--- __tests__/recipeParser.test.ts
+++ __tests__/recipeParser.test.ts
@@ -5,7 +5,10 @@ import mustardPorkTenderloinResultData from "@/testData/getRecipeObject/mustardP
 import mustardPorkTenderloin from '@/testData/scraper/mustardPorkTenderloin.json'
 import tuscanWhiteBeanData from '@/testData/getRecipeObject/tuscanWhiteBean.json'
 import tuscanWhiteBean from '@/testData/scraper/tuscanWhiteBean.json'
-
+import chickenCacciatoreData from '@/testData/getRecipeObject/chickenCacciatore.json'
+import chickenCacciatore from '@/testData/scraper/chickenCacciatore.json'
+import cauliflowerSaladData from '@/testData/getRecipeObject/cauliflowerSalad.json'
+import cauliflowerSalad from '@/testData/scraper/cauliflowerSalad.json'
 
 // Bon Appetit
 test('gets Gochujang Chicken recipe object', () => {
@@ -25,5 +28,18 @@ test('gets Mustard-Glazed Pork Tenderloin recipe object', () => {
 test('gets Tuscan White Bean Dish recipe object', () => {
   const expected = tuscanWhiteBeanData;
   const actual = getRecipeObject(tuscanWhiteBean);
+  expect(actual).toEqual(expected)
+})
+
+// desert for two
+test('gets Chicken Cacciatore recipe object', () => {
+  const expected = chickenCacciatoreData;
+  const actual = getRecipeObject(chickenCacciatore)
+  expect(actual).toEqual(expected)
+})
+
+test('gets Cauliflower Salad recipe object', () => {
+  const expected = cauliflowerSaladData;
+  const actual = getRecipeObject(cauliflowerSalad);
   expect(actual).toEqual(expected)
 })
