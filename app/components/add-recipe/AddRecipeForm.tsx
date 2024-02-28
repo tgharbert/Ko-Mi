2187004f063df8deb3017e6aaff0fe4c79ae0d08
Thomas Harbert <tgharbert3@gmail.com>
2024-02-28 10:29:57 -0600

    added form toggle between manual and url recipe entries

---
+++ app/components/add-recipe/AddRecipeForm.tsx
@@ -0,0 +1,27 @@
+"use client";
+import RecipeURLForm from "./RecipeURLForm";
+import { useState } from "react";
+import Switch from "@mui/material/Switch";
+import ManualForm from "./manualform/ManualForm";
+
+const AddRecipeForm = () => {
+  // use state to conditionally render which form to use, url or manual
+  const [isManual, setIsManual] = useState(false);
+
+  const onToggle = () => {
+    setIsManual(!isManual);
+  };
+
+  return (
+    <div>
+      <div>
+        <label>Enter URL</label>
+        <Switch onChange={onToggle} />
+        <label>Enter Recipe Manually</label>
+      </div>
+      {isManual ? <ManualForm /> : <RecipeURLForm />}
+    </div>
+  );
+};
+
+export default AddRecipeForm;
