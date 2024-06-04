82e0ac78865db839de7a93ad0ebe65b6c7bc7880
Thomas Harbert <tgharbert3@gmail.com>
2024-06-04 17:54:44 -0500

    friends change

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -1,5 +1,14 @@
+import Header from "../components/Header";
+
 const Friends = () => {
-  return <div>Hello future friends component!</div>;
+  return (
+    <div className="text-center pb-10">
+      <div className="-mt-12">
+        <Header />
+      </div>
+      <div>Hello future friends component!</div>
+    </div>
+  );
 };
 
 export default Friends;
