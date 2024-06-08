6b7d4a58294ca0f8c06c16ff9b49c042b84c7695
Thomas Harbert <tgharbert3@gmail.com>
2024-06-08 18:02:57 -0500

    fixed modal changes from bad dep branch

--- app/friends/components/Requests.tsx
+++ app/friends/components/Requests.tsx
@@ -1,45 +1,31 @@
 "use client";
-import { useState, useEffect } from "react";
 import UserList from "./UserList";
 import Search from "./Search";
-import LoadingPage from "@/app/loading";
-import addFriend from "@/lib/addFriend";
 
-const Requests = ({ getAllRequests }: { getAllRequests: Function }) => {
-  const [requests, setRequests] = useState<Friend[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const getRequestsData = async () => {
-    let usersData: Friend[] = await getAllRequests();
-    setRequests(usersData);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getRequestsData();
-  }, []);
-
-  const loadFriends = async (userId: string) => {
-    setIsLoading(true);
-    const friends = await addFriend(userId);
-    setRequests(friends);
-    setIsLoading(false);
-  };
-
+const Requests = ({
+  requests,
+  loadFriends,
+  isLoading,
+}: {
+  getAllRequests: Function;
+  requests: Friend[];
+  loadFriends: Function;
+  isLoading: boolean;
+}) => {
   return (
     <div>
-      {isLoading ? (
-        <LoadingPage />
-      ) : (
-        <div>
-          <div className="pt-4">
-            <Search loadFriends={loadFriends} />
-          </div>
-          <div>
-            <UserList requests={requests} loadFriends={loadFriends} />
-          </div>
+      <div>
+        <div className="pt-4">
+          <Search loadFriends={loadFriends} />
         </div>
-      )}
+        <div>
+          <UserList
+            requests={requests}
+            loadFriends={loadFriends}
+            isLoading={isLoading}
+          />
+        </div>
+      </div>
     </div>
   );
 };
