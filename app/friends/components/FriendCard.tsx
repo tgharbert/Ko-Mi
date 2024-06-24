a44283295c59110c02e40f35894ff614350cff81
Thomas Harbert <tgharbert3@gmail.com>
2024-06-24 12:05:24 -0500

    restyled friends list

--- app/friends/components/FriendCard.tsx
+++ app/friends/components/FriendCard.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 
 const FriendCard = ({ friend }: { friend: User }) => {
   return (
-    // <span className="flexbox sm:items-center sm:justify-center">
     <div className="flexbox overflow-y-auto mx-4 px-4 text-left border-2  border-black rounded-lg h-auto mt-4 bg-tertiary text-black w-80 sm:w-1/5 pt-4 pb-4">
       <span className="text-lg ">
         <span className="mt-4 mb-4">
@@ -17,7 +16,6 @@ const FriendCard = ({ friend }: { friend: User }) => {
         </span>
       </span>
     </div>
-    // </span>
   );
 };
 
