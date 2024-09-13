b0ee94e7fc4a19a09b0c868a971df6eb6c768820
Thomas Harbert <tgharbert3@gmail.com>
2024-09-13 09:52:34 -0500

    added about page to menu, further styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ import verifyUser from "@/utils/verifyUser";
 import Link from "next/link";
 
 const Friends = async () => {
-  await verifyUser();
+  const user = await verifyUser();
 
   return (
     <div className="text-center pb-10">
@@ -18,18 +18,18 @@ const Friends = async () => {
           <div className="-mt-40 bg-tertiary text-black sm:mx-40 pt-4 pb-4 rounded-lg border-2 border-black pr-4 pl-4 ">
             <p className="text-xl pb-3 font-bold">What is Ko-Mi?</p>
             <p className="pb-3">
-              Hi, my name is Thomas and Ko-Mi is my creation!
+              Hi <span className="italic">{user?.name}</span>, my name is Thomas
+              and Ko-Mi is my creation!
             </p>
-            <p className="pb-3">
-              As a passionate home cook and engineer, I wanted to create a
+            <p className="pb-3 xl:pr-20 xl:pl-20">
+              As both a passionate home cook and engineer, I wanted to create a
               simple, streamlined application that would help me keep track of
-              my favorite recipes, help me build shopping lists, and share my
-              passion with other home cooks. In building this project, I wanted
-              to create something that was intuitive for anybody to use.
+              my favorite recipes, build shopping lists, and share my passion
+              with other home cooks.
             </p>
-            <p className="pb-3">
+            <p className="pb-3 xl:pr-20 xl:pl-20">
               Ko-Mi is a PWA (Progressive Web App), which means that you can
-              download it onto your mobile devices just like any other app. Your
+              download it onto your mobile devices just like any other app! Your
               internet browser should have a process that allows you to download
               the page. For more information see this link:
             </p>
