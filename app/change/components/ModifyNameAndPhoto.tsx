5ac00f03f21c53b9154d28fd2ad81fcadbb2cb16
Thomas Harbert <tgharbert3@gmail.com>
2024-09-16 11:22:00 -0500

    issues...

--- app/change/components/ModifyNameAndPhoto.tsx
+++ app/change/components/ModifyNameAndPhoto.tsx
@@ -1,62 +1,112 @@
+"use client";
 import Image from "next/image";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import updateName from "../data/updateName";
 import updatePhoto from "../data/updatePhoto";
 import { revalidatePath } from "next/cache";
 import { supabase } from "@/lib/supabase";
+import { useState, useEffect } from "react";
 
-const ModifyNameAndPhoto = async ({ recipe }: { recipe: Recipe }) => {
-  async function modifyNameAndPhoto(formData: FormData) {
-    "use server";
-    console.log("submitted name and photo");
+const ModifyNameAndPhoto = ({ recipe }: { recipe: Recipe }) => {
+  const [imagePreview, setImagePreview] = useState<string | null | ArrayBuffer>(
+    null
+  );
+
+  // useEffect(() => {
+  //   const handleFileInputChange = () => {
+  //     if (!recipe.image) {
+  //       return;
+  //     }
+  //     const file: any = recipe.image;
+  //     if (file instanceof Blob) {
+  //       const reader = new FileReader();
+  //       reader.onloadend = () => {
+  //         setImagePreview(reader.result);
+  //       };
+  //       reader.readAsDataURL(file);
+  //     }
+  //   };
+  //   handleFileInputChange();
+  // }, [recipe.image]);
+
+  const handleRecipeSubmission = async () => {
+    // here is where the problem lies. I cannot pass the photo file to the backend...
+    // i'm hacking around it by copying the recipe object and giving it an any type
+    // then reassigning the photoFile key to a string of imagePreview.
+    // let customRecipe: any = recipe;
+    console.log("HIIIIIITTTTTT");
     try {
-      const rawFormData = {
-        name: formData.get("name") as string,
-        photo: formData.get("photo") as File,
-      };
-
-      let newName = rawFormData.name;
-      if (newName) {
-        newName = newName.toString();
-        await updateName(recipe.id, newName);
-      }
-
-      if (rawFormData.photo.size === 0) {
-        return;
-      }
       const filename = `${recipe.name}Photo`;
-      // delete photo file
-      // const { error } = await supabase.storage.from("images").remove([filename]);
-      // if (error) {
-      //   console.error("error deleting old photo: ", error);
-      //   return;
-      // }
-
       const recipeAddress = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/${filename}`;
-      if (rawFormData.photo instanceof File) {
-        // const filename = `${recipe.name}Photo`;
-        const { error } = await supabase.storage
-          .from("images")
-          .upload(filename, rawFormData.photo, {
-            contentType: rawFormData.photo.type,
-            cacheControl: "3600",
-            upsert: true,
-          });
-        if (error) {
-          console.error("error from upload: ", error);
-          return;
-        }
-        updatePhoto(recipe.id, recipeAddress);
+      const { error } = await supabase.storage
+        .from("images")
+        .upload(filename, recipe.image, {
+          cacheControl: "3600",
+          upsert: true,
+        });
+      if (error) {
+        console.error("error from upload: ", error);
       }
-      revalidatePath(`/change/${recipe.id}`);
+      // customRecipe.image = recipeAddress;
+      // await addCustomRecipe(recipe);
+      updatePhoto(recipe.id, recipeAddress);
     } catch (error) {
-      console.error("error on submission: ", error);
-      return;
+      console.error("error attempting to upload: ", error);
     }
-  }
+  };
+
+  // async function modifyNameAndPhoto(formData: FormData) {
+  //   "use server";
+  //   console.log("submitted name and photo");
+  //   try {
+  //     const rawFormData = {
+  //       name: formData.get("name") as string,
+  //       photo: formData.get("photo") as File,
+  //     };
+
+  //     let newName = rawFormData.name;
+  //     if (newName) {
+  //       newName = newName.toString();
+  //       await updateName(recipe.id, newName);
+  //     }
+
+  //     if (rawFormData.photo.size === 0) {
+  //       return;
+  //     }
+  //     const filename = `${recipe.name}Photo`;
+  //     // delete photo file
+  //     // const { error } = await supabase.storage.from("images").remove([filename]);
+  //     // if (error) {
+  //     //   console.error("error deleting old photo: ", error);
+  //     //   return;
+  //     // }
+
+  //     const recipeAddress = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/${filename}`;
+  //     if (rawFormData.photo instanceof File) {
+  //       // const filename = `${recipe.name}Photo`;
+  //       const { error } = await supabase.storage
+  //         .from("images")
+  //         .upload(filename, rawFormData.photo, {
+  //           contentType: rawFormData.photo.type,
+  //           cacheControl: "3600",
+  //           upsert: true,
+  //         });
+  //       if (error) {
+  //         console.error("error from upload: ", error);
+  //         return;
+  //       }
+  //       updatePhoto(recipe.id, recipeAddress);
+  //     }
+  //     revalidatePath(`/change/${recipe.id}`);
+  //   } catch (error) {
+  //     console.error("error on submission: ", error);
+  //     return;
+  //   }
+  // }
 
   return (
-    <form action={modifyNameAndPhoto} className="pb-6">
+    // <form action={modifyNameAndPhoto} className="pb-6">
+    <form className="pb-6" onSubmit={() => handleRecipeSubmission}>
       <div className="pb-4 pt-4">
         <input
           className="text-black text-center font-bold rounded-lg px-4 pt-1 pb-1 height-auto resize-y border-2 border-primary w-full sm:w-96"
@@ -67,6 +117,15 @@ const ModifyNameAndPhoto = async ({ recipe }: { recipe: Recipe }) => {
         />
       </div>
       <div className="flex justify-center pb-4">
+        {typeof imagePreview === "string" && (
+          <Image
+            width="400"
+            height="400"
+            src={imagePreview}
+            alt="recipe-photo"
+            className="rounded-lg"
+          />
+        )}
         <Image
           className="rounded-lg"
           src={recipe.image}
