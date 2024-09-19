728ee66be1c24f8ccbf82718cadfcc15f497e870
Thomas Harbert <tgharbert3@gmail.com>
2024-09-19 17:18:06 -0500

    file cleanup

--- app/change/components/ModifyNameAndPhoto.tsx
+++ app/change/components/ModifyNameAndPhoto.tsx
@@ -21,15 +21,12 @@ const ModifyNameAndPhoto = ({
   const [selectedImage, setSelectedImage] = useState<any>(null);
   const [previewUrl, setPreviewUrl] = useState<string>("");
 
-  console.log(selectedImage);
-
   // Handle image selection
   const handleImageChange = (e: any) => {
     const file = e.target.files[0]; // Get the selected file
     if (file) {
       setSelectedImage(file); // Set the file in state
       setPreviewUrl(URL.createObjectURL(file)); // Generate a preview URL for the image
-      // console.log("previewURL: ", previewUrl);
     }
   };
 
@@ -43,7 +40,6 @@ const ModifyNameAndPhoto = ({
     }
     const filename = `${recipe.name}Photo-${new Date().getTime()}`;
     const recipeAddress = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/${filename}`;
-    // await supabase.storage.from("images").update(filename, selectedImage);
     const { error } = await supabase.storage
       .from("images")
       .upload(filename, selectedImage, {
@@ -84,43 +80,15 @@ const ModifyNameAndPhoto = ({
           />
         </div>
         <div className="flex justify-center pb-4">
-          {previewUrl ? (
-            <Image
-              width={300}
-              height={300}
-              src={previewUrl}
-              alt={`preview of ${recipe.name}`}
-              className="rounded-lg"
-            />
-          ) : (
-            <Image
-              className="rounded-lg"
-              src={recipe.image}
-              alt={`photo of ${recipe.name}`}
-              width={300}
-              height={300}
-            ></Image>
-          )}
+          <Image
+            className="rounded-lg"
+            src={previewUrl ? previewUrl : recipe.image}
+            alt={`photo of ${recipe.name}`}
+            width={300}
+            height={300}
+          ></Image>
         </div>
-        {/* <div className="relative inline-block cursor-pointer ">
-        <input
-          type="file"
-          name="photo"
-          // className=" absolute inset-0 w-full h-full cursor-pointer "
-          className="opacity-0 absolute inset-0 w-full h-full cursor-pointer "
-          onChange={handleImageChange}
-        ></input>
-        <label
-          htmlFor="file-upload"
-          className="cursor-pointer bg-secondary text-white hover:bg-lime-600 ml-2 px-4 py-2 rounded-lg italic"
-        >
-          <CloudUploadIcon className="mr-2 pb-1 cursor-pointer" />
-          Click to Upload Photo
-        </label>
-      </div> */}
-
         <div className="mb-4">
-          {/* <p>Upload a Photo:</p> */}
           <Button
             component="label"
             role={undefined}
@@ -145,12 +113,7 @@ const ModifyNameAndPhoto = ({
         </div>
 
         <div className="">
-          <Button
-            className="bg-lime-500 px-4"
-            variant="contained"
-            color="lime"
-            // className="font-bold ml-2 pt-1 pb-1 text-bold bg-secondary hover:bg-lime-600 rounded-lg px-4"
-          >
+          <Button className="bg-lime-500 px-4" variant="contained" color="lime">
             Update Name and Photo
           </Button>
         </div>
